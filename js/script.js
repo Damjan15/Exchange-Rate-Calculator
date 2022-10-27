fb9c62ca45fd758bcb5e6d87f20dc1d6a991aa5e
Damjan15 <stamenovdamjan20@gmail.com>
2022-10-27 19:04:47 +0200

    Calculate functionality added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,38 @@
-console.log("Hello World!");
+// ====== VARIABLES ======
+const currencyEl_one = document.getElementById("currency-one");
+const currencyEl_two = document.getElementById("currency-two");
+const amountEl_one = document.getElementById("amount-one");
+const amountEl_two = document.getElementById("amount-two");
+const rateEl = document.getElementById("rate");
+const swap = document.getElementById("swap");
+
+
+// Fetch exchange rates and update the DOM
+async function calculate() {
+    const currency_one = currencyEl_one.value;
+    const currency_two = currencyEl_two.value;
+
+    const currencies = await fetch(`https://v6.exchangerate-api.com/v6/8c9ae31cb45c5c8e19c3cce6/latest/${currency_one}`);
+    const data = await currencies.json();
+    const rate = data.conversion_rates[currency_two]
+    
+    rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
+
+    amountEl_two.value = (amountEl_one * rate).toFixed(2);
+    
+}
+
+
+
+// ====== EVENT LISTENERS ======
+currencyEl_one.addEventListener("change", calculate);
+amountEl_one.addEventListener("input", calculate);
+currencyEl_two.addEventListener("change", calculate);
+amountEl_two.addEventListener("input", calculate);
+swap.addEventListener("click", () => {
+    const temp = currencyEl_one.value;
+    currencyEl_one.value = currencyEl_two.value;
+    currencyEl_two.value = temp;
+
+    calculate();
+})
